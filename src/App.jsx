f2d2f63373d61dc76c0e9c3281b44c95ecdf0c2b
untitlez <tleza_championship2@hotmail.com>
2025-01-13 20:06:33 +0700

    Update changes before deployment

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ function App() {
   return (
     <>
       <div className="home Relative" >
-        <Router basename="/My-Portfolio">
+        <Router>
           <Navbar />
           <Routes>
               <Route path="/" element={<Home />} />
